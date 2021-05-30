70735ae52b964086d56e63b8eb19bd535776f2ac
Santiago Salgado <jssalgadog@gmail.com>
2021-05-29 21:55:28 -0500

    code the script to send the data to the endpoint

--- server.py
+++ server.py
@@ -4,15 +4,27 @@ import numpy as np
 
 from flask import Flask
 from flask import jsonify
+from flask import render_template
+from flask import request
 import json
 
 app = Flask(__name__)
 
 #POSTMAN PARA PRUEBAS
-@app.route('/predict', methods = ['GET'])
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods = ['GET', 'POST'])
 def predict():
-    X_test = np.array([4, 1, 39, 1, 0, 1, 0, 0, 0, 0, 0])
-    prediction = int(model.predict(X_test.reshape(1,-1))[0])
+    if request.method == 'POST':
+        print(request.data)
+        prediction = 'Hola'
+    else:
+        X_test = np.array([4, 1, 39, 1, 0, 1, 0, 0, 0, 0, 0])
+        prediction = int(model.predict(X_test.reshape(1,-1))[0])
     return jsonify(
                     {"Prediccion": prediction}
                 )
---
+++ templates/scripts.js
@@ -0,0 +1,28 @@
+
+const sendForm = document.getElementById( 'SendForm');
+
+sendForm.addEventListener('click', sendFormValidation);
+
+function sendFormValidation () {+
+    alert("Hi")
+    // const url = 'https://ancient-cove-37870.herokuapp.com/sign-up';
+    // const uname = document.getElementById("sgname").value;
+    // const usern= document.getElementById("sgusername").value;
+    // const uemail = document.getElementById("sgemail").value;
+    // const passn = document.getElementById("sgpassword").value;
+    // let data = {
+    //     "name": uname,
+    //     "username": usern,
+    //     "email": uemail,
+    //     "password": passn
+    // }
+    // postData(url, data)
+    // .then(data => {
+    //     if(data.message){
+    //         signUpScreen.style.display = 'none';
+    //         InitialScreen.style.display = 'block';
+    //     } else {
+    //         alert("Not valid user")
+    //     }
+    // });
+}
