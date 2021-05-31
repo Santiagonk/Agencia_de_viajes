feea80991a7b9d68fb33636e2b0e2def08ce319a
Santiago Salgado <jssalgadog@gmail.com>
2021-05-31 07:11:55 -0500

    Implemented a app that predict based in the inputs

--- server.py
+++ server.py
@@ -8,6 +8,53 @@ from flask import render_template
 from flask import request
 import json
 
+def toPredictedArray(dataInput):
+  dataString = str(dataInput)
+  dataString = dataString.strip('b')[1:-1]
+  dataString = dataString.strip('{').strip('}').replace('"','')
+  dataString = dataString.split(',')
+
+  dictionary = {}
+  for i in dataString:
+    dictionary[i.split(':')[0]] = i.split(':')[1]
+  
+  y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+  for key in dictionary.keys():
+    if key == 'destino':
+      if dictionary[key] == 'COL':
+        y[4] = 1
+      elif dictionary[key] == 'ES':
+        y[5] = 1
+      elif dictionary[key] == 'IT':
+        y[6] = 1
+      elif dictionary[key] == 'NL':
+        y[7] = 1
+      elif dictionary[key] == 'PE':
+        y[8] = 1
+      elif dictionary[key] == 'UK':
+        y[9] = 1
+      elif dictionary[key] == 'US':
+        y[10] = 1
+    
+    if key == 'duracionEstadia':    
+      y[0] = int(dictionary[key])
+    
+    if key == 'genero':
+      if dictionary[key] == 'F':
+        y[1] = 1
+    
+    if key == 'edad':
+      y[2] = int(dictionary[key])
+    
+    if key == 'ninos':
+      if dictionary[key] == 'SI':
+        y[3] = 1
+  
+  return y
+
+
+
 app = Flask(__name__)
 
 #POSTMAN PARA PRUEBAS
@@ -20,8 +67,10 @@ def index():
 @app.route('/predict', methods = ['GET', 'POST'])
 def predict():
     if request.method == 'POST':
-        print(request.data)
-        prediction = 'Hola'
+        dataInput = request.data
+        X_test = np.array(toPredictedArray(dataInput))
+        prediction = int(model.predict(X_test.reshape(1,-1))[0])
+        print(prediction)
     else:
         X_test = np.array([4, 1, 39, 1, 0, 1, 0, 0, 0, 0, 0])
         prediction = int(model.predict(X_test.reshape(1,-1))[0])
