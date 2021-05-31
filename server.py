52d13360c81e6a335c84027e7d66e83a2c2bb80f
Santiago Salgado <jssalgadog@gmail.com>
2021-05-31 07:24:26 -0500

    put a function into the utils file

--- server.py
+++ server.py
@@ -1,59 +1,12 @@
 import joblib
 import numpy as np
-
+from utils import toPredictedArray
 
 from flask import Flask
 from flask import jsonify
 from flask import render_template
 from flask import request
 import json
-
-def toPredictedArray(dataInput):
-  dataString = str(dataInput)
-  dataString = dataString.strip('b')[1:-1]
-  dataString = dataString.strip('{').strip('}').replace('"','')
-  dataString = dataString.split(',')
-
-  dictionary = {}
-  for i in dataString:
-    dictionary[i.split(':')[0]] = i.split(':')[1]
-  
-  y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-  for key in dictionary.keys():
-    if key == 'destino':
-      if dictionary[key] == 'COL':
-        y[4] = 1
-      elif dictionary[key] == 'ES':
-        y[5] = 1
-      elif dictionary[key] == 'IT':
-        y[6] = 1
-      elif dictionary[key] == 'NL':
-        y[7] = 1
-      elif dictionary[key] == 'PE':
-        y[8] = 1
-      elif dictionary[key] == 'UK':
-        y[9] = 1
-      elif dictionary[key] == 'US':
-        y[10] = 1
-    
-    if key == 'duracionEstadia':    
-      y[0] = int(dictionary[key])
-    
-    if key == 'genero':
-      if dictionary[key] == 'F':
-        y[1] = 1
-    
-    if key == 'edad':
-      y[2] = int(dictionary[key])
-    
-    if key == 'ninos':
-      if dictionary[key] == 'SI':
-        y[3] = 1
-  
-  return y
-
-
 
 app = Flask(__name__)
 
@@ -70,7 +23,6 @@ def predict():
         dataInput = request.data
         X_test = np.array(toPredictedArray(dataInput))
         prediction = int(model.predict(X_test.reshape(1,-1))[0])
-        print(prediction)
     else:
         X_test = np.array([4, 1, 39, 1, 0, 1, 0, 0, 0, 0, 0])
         prediction = int(model.predict(X_test.reshape(1,-1))[0])
--- utils.py
+++ utils.py
@@ -15,3 +15,48 @@ class Utils:
     def model_export(self, model, score):
         print(score)
         joblib.dump(model, './models/best_model.pkl')
+
+def toPredictedArray(dataInput):
+  dataString = str(dataInput)
+  dataString = dataString.strip('b')[1:-1]
+  dataString = dataString.strip('{').strip('}').replace('"','')
+  dataString = dataString.split(',')
+
+  dictionary = {}
+  for i in dataString:
+    dictionary[i.split(':')[0]] = i.split(':')[1]
+  
+  y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+  for key in dictionary.keys():
+    if key == 'destino':
+      if dictionary[key] == 'COL':
+        y[4] = 1
+      elif dictionary[key] == 'ES':
+        y[5] = 1
+      elif dictionary[key] == 'IT':
+        y[6] = 1
+      elif dictionary[key] == 'NL':
+        y[7] = 1
+      elif dictionary[key] == 'PE':
+        y[8] = 1
+      elif dictionary[key] == 'UK':
+        y[9] = 1
+      elif dictionary[key] == 'US':
+        y[10] = 1
+    
+    if key == 'duracionEstadia':    
+      y[0] = int(dictionary[key])
+    
+    if key == 'genero':
+      if dictionary[key] == 'F':
+        y[1] = 1
+    
+    if key == 'edad':
+      y[2] = int(dictionary[key])
+    
+    if key == 'ninos':
+      if dictionary[key] == 'SI':
+        y[3] = 1
+  
+  return y
